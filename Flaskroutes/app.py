c815089758c4e5eb8056b95f88bae4c9e78e070f
Jamalh8 <jamalh.888@gmail.com>
2022-02-07 16:33:56 +0000

    Commiting everything

---
+++ Flaskroutes/app.py
@@ -0,0 +1,41 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# @app.route('/home')
+# def home():
+#     return 'This is the home page'
+
+# @app.route('/about')
+# def about():
+#     return 'This is the about page'
+
+# if __name__ == "__main__":
+#     app.run(debug=True, host='0.0.0.0', port=5000)
+
+# from flask import Flask, url_for
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# @app.route('/home')
+# def home():
+#     return 'This is the home page'
+
+# @app.route('/square/<int:num>')
+# def square(num):
+#     answer = num*num
+#     return str(answer)
+
+# @app.route('/about')
+# def about():
+#     return 'This is the about page'
+
+# @app.route('/test')
+# def test():
+#     return 'This is a test URL'
+
+# if __name__ == "__main__":
+#     app.run(debug=True, host='0.0.0.0', port=5000)
+
