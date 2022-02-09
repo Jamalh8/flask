89903fc52510e8340ca059f54d41a84649dae73c
Jamalh8 <jamalh.888@gmail.com>
2022-02-09 14:23:31 +0000

    Updated 09.02.22 14.23

--- flask-templating/app.py
+++ flask-templating/app.py
@@ -1,3 +1,4 @@
+from http.client import ResponseNotReady
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,6 +10,10 @@ def ben():
 @app.route('/harry')
 def harry():
     return render_template('harry.html')
+
+@app.route('/users')
+def users():
+    return render_template ('users.html', users=["ben", "harry", "bob", "jay", "matt", "bill"])
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
