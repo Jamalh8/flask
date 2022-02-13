641dfebdd138d4f6d1bd9b8e50b7deff01a4637a
Jamalh8 <jamalh.888@gmail.com>
2022-02-13 16:16:00 +0000

    Updated 13.02.22 16:15

--- todo/application/routes.py
+++ todo/application/routes.py
@@ -3,3 +3,7 @@ from application.models import Tasks
 from application.forms import CreateForm, UpdateForm
 from flask import render_template, redirect, url_for, request
 
+@app.route('/')
+@app.route('read'):
+def read():
+    
